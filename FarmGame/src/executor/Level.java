919502541fee5daf89b90dd3b21eecc246456c6d
Kokipoki <karimgame1999@yahoo.com>
2021-11-27 00:54:44 -0500

    Level class and interpreter beginning

---
+++ FarmGame/src/executor/Level.java
@@ -0,0 +1,53 @@
+package executor;
+
+import executor.animal.Animal;
+import java.util.ArrayList;
+
+public class Level {
+
+    private ArrayList<Location> locations;
+    private ArrayList<Farmer> farmers;
+    private ArrayList<Animal> animals;
+    
+    public void addLocation(Location location) {
+        locations.add(location);
+    }
+    
+    public void addFarmer(Farmer farmer) {
+        farmers.add(farmer);
+    }
+    
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+    }
+    
+    public Location findLocation(String name) {
+        Location location = null;
+        for (Location l : locations) {
+            if (l.getName().equals(name)) {
+                location = l;
+            }
+        }
+        return location;
+    }
+    
+    public Farmer findFarmer(String name) {
+        Farmer farmer = null;
+        for (Farmer f : farmers) {
+            if (f.getName().equals(name)) {
+                farmer = f;
+            }
+        }
+        return farmer;
+    }
+    
+    public Animal findAnimal(String type) {
+        Animal animal = null;
+        for (Animal a : animals) {
+            if (a.getClass().getSimpleName().equals(type)) {
+                animal = a;
+            }
+        }
+        return animal;
+    }
+}
--- FarmGame/src/interpreter/Interpreter.java
+++ FarmGame/src/interpreter/Interpreter.java
@@ -1,17 +1,61 @@
 package interpreter;
 
+import executor.Level;
+
 public class Interpreter {
-    public void interpretSentence(String sentence) {
-        // Split sentence into words
-        String[] words = sentence.toLowerCase().split(" ");
+    public void interpretSentence(String sentence, Level level) {
+        // Split sentence into words, lowercased and punctuation removed
+        String[] words = sentence.replaceAll("[^a-zA-Z ]", "").toLowerCase().split(" ");
         
-        // First word must be "farmer"
-        if (!words[0].equals("farmer")) {
-            System.out.println("Invalid Sentence: Sentences must start with the word \"Farmer\"");
+        // Remove extra words like "the" "of" "is" "going" "to"
+        // TO DO
+        // Maybe conver to arraylist
+        
+        // Shortest valid sentence is 3 words long.
+        if (words.length < 3) {
+            System.out.println("Invalid Sentence: Sentences must be at least 3 words long!");
             return;
         }
         
+        // First word must be "farmer"
+        if (!words[0].equals("farmer")) {
+            System.out.println("Invalid Sentence: Sentences must start with the word \"Farmer\".");
+            return;
+        }
         
+        // Find a farmer by their name
+        if (level.findFarmer(words[1]) == null) {
+            System.out.println("Invalid Sentence: The second word of the sentence must be the name of a farmer that exists!");
+            return;
+        }
         
+        // Action specifier
+        // Valid actions: moves, speaks, nurses, cleans, milks
+        String action = words[2];
+        switch(action) {
+            case "moves":
+                //
+                break;
+            case "speaks":
+                //
+                break;
+            case "nurses":
+                //
+                break;
+            case "feeds":
+                //
+                break;
+            case "cleans":
+                //
+                break;
+            case "milks":
+                //
+                break;
+        }
+    }
+    
+    // Checks if an "if" statement is true
+    public boolean conditionChecker(String condition, Level level) {
+        return false;
     }
 }
